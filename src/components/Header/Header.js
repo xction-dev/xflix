2eea3c1897a2764c32e6055c113f6c2f8ae3e231
nuagenic <nuagenic@gmail.com>
2023-08-22 02:24:34 +0900

    refact: Delete <button> and <div> child tag in <Link>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,8 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <div className="header-container">
-            <Link href="/dashboard" style={{ textDecoration: "none", color: "blue"}}>
-                <div className="logo">XFLIX</div>
-            </Link>
-            <Link href="/login">
-                <button className="login-btn">로그인</button>
-            </Link>
+            <Link href="/dashboard" className="logo">XFLIX</Link>
+            <Link href="/login" className="login-btn">로그인</Link>
         </div>
     )
 }
