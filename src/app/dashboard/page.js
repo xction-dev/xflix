9141931692f0c0cca09fa0c15da5e8fcd329a7dc
nuagenic <nuagenic@gmail.com>
2023-08-24 21:49:04 +0900

    feat: Connect dashboard with curations api

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/Header/Header"
 import DashboardContainer from "@/components/Curations/DashboardContainer/DashboardContainer"
 
 export default function Dashboard() {
-
+    
     return (
         <>
             <Header />
@@ -10,6 +10,3 @@ export default function Dashboard() {
         </>
     )
 }
-
-// 1. curations.json의 제목을 갖고 와서 map으로 리스트
-// 2. 각 curation 제목의 하단에 content를 리스트 형태로 보여줌
