c236721cf21cc6e2adc2a6bfc2f6d7611f982bd6
designDefined <designdefined.by@gmail.com>
2023-08-28 11:51:00 +0900

    fix: Reset home page component

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [answer, setAnswer] = useState("waiting for fetch");
-  useEffect(() => {
-    fetch("/api/user/login", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "test02",
-        password: "password02",
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => setAnswer(JSON.stringify(data)));
-  }, []);
-  return <>{answer}</>;
+  return <>Home Page</>;
 }
