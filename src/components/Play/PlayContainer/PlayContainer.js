df9cb2719e3cd36a50d4f6fa8093af9e1ae85998
임승현 <limseunghyun@imseunghyeon-ui-MacBookAir.local>
2023-08-28 23:26:20 +0900

    feat: css flex로 완성

--- src/components/Play/PlayContainer/PlayContainer.js
+++ src/components/Play/PlayContainer/PlayContainer.js
@@ -1,13 +1,14 @@
 import Player from "src/components/Play/Player/Player.js";
 import Comments from "src/components/Play/Comments/Comments.js";
+import "./PlayContainer.css";
 
 export default function PlayContainer(props) {
   var id = props.id;
 
   return (
-    <div>
-      <Player playerId={id} />
-      <Comments commentsId={id} />
+    <div className="play-container">
+      <Player playerId={id} className="player-component" />
+      <Comments commentsId={id} className="comments-component" />
     </div>
   );
 }
