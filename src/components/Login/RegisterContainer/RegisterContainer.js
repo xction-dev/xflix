465ffcb92c66dea4b15919e7d8e101cbff44220f
nuagenic <nuagenic@gmail.com>
2023-09-01 23:26:33 +0900

    feat: Try to add register feature in 'RegisterContainer.js' (not working yet)

--- src/components/Login/RegisterContainer/RegisterContainer.js
+++ src/components/Login/RegisterContainer/RegisterContainer.js
@@ -7,12 +7,66 @@ import SubmitButton from "../../SubmitButton/SubmitButton";
 import "./RegisterContainer.css";
 
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function RegisterContainer() {
   const [IDRegisterValue, setIDRegisterValue] = useState("");
   const [PWRegisterValue, setPWRegisterValue] = useState("");
   const [PWConfirmValue, setPWConfirmValue] = useState("");
   const [nameValue, setNameValue] = useState("");
+  const [registerSuccess, setRegisterSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const router = useRouter();
+
+  const handleRegister = async () => {
+    console.log("hi");
+    const apiUrl = "/api/users/register"; // 서버 API 엔드포인트 URL
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameValue,
+          username: IDRegisterValue,
+          password: PWRegisterValue,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.isRegisterSuccess) {
+          setRegisterSuccess(true);
+        }
+      } else {
+        const data = await response.json();
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      setErrorMessage("An error occurred while registering.");
+    }
+  };
+
+  // '가입' 버튼의 Action을 회원가입 성공 여부에 따라 제어하는 함수
+  const handleButtonAction = async () => {
+    if (registerSuccess === true) {
+      // 성공한 경우 다른 페이지로 이동
+      router.push("/login");
+    } else {
+      // 아직 작업을 실행하지 않은 경우
+      try {
+        await handleRegister(); // 회원가입 요청 완료를 기다림
+        setRegisterSuccess(true);
+      } catch (error) {
+        // 오류 처리
+        console.error("An error occurred:", error);
+      }
+    }
+  };
 
   return (
     <div className="register-container">
@@ -32,7 +86,9 @@ export default function RegisterContainer() {
         setInputValue={setPWConfirmValue}
       />
       <NameInput inputValue={nameValue} setInputValue={setNameValue} />
-      <SubmitButton status="register" />
+      <SubmitButton onClick={handleButtonAction} status="register" />
+      {registerSuccess && <p>회원가입이 성공적으로 완료되었습니다.</p>}
+      {errorMessage && <p>오류: {errorMessage}</p>}
     </div>
   );
 }
--- src/components/SubmitButton/SubmitButton.js
+++ src/components/SubmitButton/SubmitButton.js
@@ -1,10 +1,9 @@
 import "./SubmitButton.css";
-import Link from "next/link";
 
 export default function SubmitButton({ status }) {
   return (
-    <Link href="/dashboard" className="submit-btn">
+    <button className="submit-btn">
       {status === "submit" ? "확인" : "가입"}
-    </Link>
+    </button>
   );
 }
