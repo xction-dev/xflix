40c626d962fa492577f778208f1abff9c99f3e11
임승현 <limseunghyun@imseunghyeon-ui-MacBookAir.local>
2023-09-05 17:48:38 +0900

    fix: still trying

--- src/components/Play/PlayContainer/PlayContainer.js
+++ src/components/Play/PlayContainer/PlayContainer.js
@@ -4,38 +4,35 @@ import Player from "src/components/Play/Player/Player.js";
 import Comments from "src/components/Play/Comments/Comments.js";
 import "./PlayContainer.css";
 import { useState, useEffect } from "react";
-import contentsData from "src/mock/contents.json";
 
 export default function PlayContainer(props) {
-  var id = props.contentId;
-  // const contentToPlay = contentsData[id];
-
   const [contentData, setContentData] = useState(null);
 
   useEffect(() => {
-    const apiUrl = "/api/contents/:contents_id"; // 서버 API 엔드포인트 URL
+    const id = props.contentId;
+    const apiUrl = `/api/contents/${id}`; // 서버 API 엔드포인트 URL
 
     fetch(apiUrl)
       .then((response) => response.json())
-      .then((Content) => {
-        setContentData(Content);
+      .then((parsedResponse) => {
+        setContentData(parsedResponse);
+        console.log(contentData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
-  console.log(contentData);
-  var contentToPlay = contentData;
-
-  if (contentToPlay.type !== "youtube") {
+  if (contentData === undefined) {
+    return <p>Loading..</p>;
+  } else if (contentData && contentData.type !== "youtube") {
     return <div>잘못된 접근입니다</div>;
   }
 
   return (
     <div className="play-container">
-      <Player content={contentToPlay} className="player-component" />
-      <Comments content={contentToPlay} className="comments-component" />
+      <Player content={contentData} className="player-component" />
+      <Comments content={contentData} className="comments-component" />
     </div>
   );
 }
